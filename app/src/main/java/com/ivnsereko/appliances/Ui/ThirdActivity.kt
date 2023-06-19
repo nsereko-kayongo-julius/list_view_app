d78d69f7650254736b587db211893ab8e829d645
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-06-19 19:07:28 +0300

    epty activity

---
+++ app/src/main/java/com/ivnsereko/appliances/Ui/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.ivnsereko.appliances.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ivnsereko.appliances.R
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
