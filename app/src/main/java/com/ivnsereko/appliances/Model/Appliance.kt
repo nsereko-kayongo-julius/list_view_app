9a0fd4af488f6e6596c3561baad6d39b72c12f6a
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-06-19 19:09:52 +0300

    data class used to create the appliances

---
+++ app/src/main/java/com/ivnsereko/appliances/Model/Appliance.kt
@@ -0,0 +1,6 @@
+package com.ivnsereko.appliances.Model
+
+data class Appliance(var description: String,
+                     var price: String,
+                     var name: String,
+                     var imageId: Int)
