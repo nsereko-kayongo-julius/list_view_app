131f30f05b70a22833bd833b10e51c4ad736cdd3
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-06-19 19:06:56 +0300

    layout file used to draw one item which will be used to populate the main list

---
+++ app/src/main/java/com/ivnsereko/appliances/Ui/ListItemActivity.kt
@@ -0,0 +1,12 @@
+package com.ivnsereko.appliances.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ivnsereko.appliances.R
+
+class ListItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_item_second)
+    }
+}
